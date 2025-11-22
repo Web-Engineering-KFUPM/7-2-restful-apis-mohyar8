00aabb2cce009af7b44061648cc35b948a2a0952
mohyar8 <mohyar44@gmail.com>
2025-11-22 23:33:59 +0300

    done

--- 7-2-restful-api/server/db.js
+++ 7-2-restful-api/server/db.js
@@ -5,9 +5,20 @@
  *    - Export connectDB() that connects Mongoose using MONGO_URL
  *    - Log success; throw on failure
  */
+
 import mongoose from "mongoose";
 
-export async function connectDB(url) {
-  await mongoose.connect(url);
-  console.log("[DB] Mongo connected");
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Mongo connected");
+  } catch (err) {
+    console.error("Connection error:", err.message);
+    throw err;
+  }
+}
+
+export async function disconnectDB() {
+  await mongoose.disconnect();
 }
--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
+
+
+// db schema
+
 const songSchema = new mongoose.Schema({
   title:  { type: String, required: true, trim: true },
   artist: { type: String, required: true, trim: true },
   year:   { type: Number, min: 1900, max: 2100 }
 }, { timestamps: true });
-
-// db schema
-
 
 export const Song = mongoose.model("Song", songSchema);
